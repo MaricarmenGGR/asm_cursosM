728780bdec4fa198c5df87ddbf397f6deaf4538c
Carlos Martínez <carlossmartinez98@gmail.com>
2020-03-24 17:42:40 -0600

    login funcional, plantilla menu

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,48 @@
-@extends('layouts.app')
-
+@extends('layouts.menu')
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
-
+<br>
+<br>
+<div class="row cursos">
+<div class="col-md-4">
+    <ul class="menu2">
+        <li><div class="card" style="width: 18rem">
+            <img class="card-img-top" src="../img/lala.jpg" alt="Card image cap">
+            <div class="card-body">
+                <h5 class="card-title">Nombre Curso</h5>
+                <p class="card-text">Descripción</p>
+                <a href="#" class="btn verde">Entrar</a>
+            </div>
+            </div></li>
+    
+    </ul>
+</div>
+<div class="col-md-4">
+    <ul class="menu2">
+        <li><div class="card" style="width: 18rem;">
+        <img class="card-img-top" src="../img/lala.jpg" alt="Card image cap">
+            <div class="card-body">
+                <h5 class="card-title">Nombre Curso</h5>
+                <p class="card-text">Descripción</p>
+                <a href="#" class="btn verde">Entrar</a>
+            </div>
+            </div></li>
+    </ul>
+</div>
+<div class="col-md-4">
+    <ul class="menu2">
+        <li>
+            <div class="card" style="width: 18rem;">
+            <img class="card-img-top" src="../img/lala.jpg" alt="Card image cap">
                 <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
+                    <h5 class="card-title">Nombre Curso</h5>
+                    <p class="card-text">Descripción</p>
+                    <a href="#" class="btn verde">Entrar</a>
                 </div>
             </div>
-        </div>
-    </div>
+        </li>
+    </ul>
+</div>
+<br>
+<a href="#" class="btn btn-secondary nuevo">Nuevo curso</a>
 </div>
 @endsection
