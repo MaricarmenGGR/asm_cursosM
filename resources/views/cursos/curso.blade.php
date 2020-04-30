939c30a030161a3350d39badf180da529fa0d3d1
asmcursos <asmcursosmac@gmail.com>
2020-04-30 14:56:14 -0500

    Programav1

--- resources/views/cursos/curso.blade.php
+++ resources/views/cursos/curso.blade.php
@@ -209,38 +209,151 @@
                         <h1>Programa del curso</h1>
                         <br>
                         <br>
-                        <div class="table-responsive">
-                            <table class="table mx-auto col-lg-10 col-md-10 col-sm-10 col-xs-10">
-                                <thead class="thead-dark ">
-                                    <tr>
-                                    <th scope="col">No.</th>
-                                    <th scope="col">Actividad</th>
-                                    <th scope="col">Hora</th>
-                                    <th scope="col">Material</th>
-                                    </tr>
+                        <div>
+                            FORMULARIO?
+                        </div>
+                        <br>
+                        <!-- Editable table -->
+                        <div class="card">
+                        <h3 class="card-header text-center font-weight-bold text-uppercase py-4">Programa</h3>
+                        <div class="card-body">
+                            <div id="table" class="table-editable">
+                            <span class="table-add float-right mb-3 mr-2"><a href="#!" class="text-success"><i
+                                    class="fas fa-plus fa-2x" aria-hidden="true"></i></a></span>
+                            <table class="table table-bordered table-responsive-md table-striped text-center">
+                                <thead>
+                                <tr>
+                                    <th class="text-center">Identificador de Curso</th>
+                                    <th class="text-center">Actividad</th>
+                                    <th class="text-center">Hora</th>
+                                    <th class="text-center">Material</th>
+                                    <th class="text-center">Ordenar</th>
+                                    <th class="text-center">Borrar</th>
+                                </tr>
                                 </thead>
                                 <tbody>
-                                    <tr>
-                                    <th scope="row">1</th>
-                                    <td><input type="text" placeholder="Actividad"></td>
-                                    <td><input type="time" placeholder="Hora"></td>
-                                    <td><input type="text" placeholder="Material"></td>
-                                    </tr>
+                                <tr>
+                                    <td class="pt-3-half" contenteditable="true">{{$curso->id}}</td>
+                                    <td class="pt-3-half" contenteditable="true">Actividad</td>
+                                    <td class="pt-3-half" contenteditable="true">Hora</td>
+                                    <td class="pt-3-half" contenteditable="true">Material</td>
+                                    <td class="pt-3-half">
+                                    <span class="table-up"><a href="#!" class="indigo-text"><i class="fas fa-long-arrow-alt-up"
+                                            aria-hidden="true"></i></a></span>
+                                    <span class="table-down"><a href="#!" class="indigo-text"><i class="fas fa-long-arrow-alt-down"
+                                            aria-hidden="true"></i></a></span>
+                                    </td>
+                                    <td>
+                                    <span class="table-remove"><button type="button"
+                                        class="btn btn-danger btn-rounded btn-sm my-0">Borrar</button></span>
+                                    </td>
+                                </tr>
                                 </tbody>
                             </table>
+                            </div>
+                        </div>
                         </div>
                         <button type="button" class="btn btn-secondary">Editar</button>
+                        <script>
+                        const $tableID = $('#table');
+                        const $BTN = $('#export-btn');
+                        const $EXPORT = $('#export');
+
+                        const newTr = `
+                        <tr class="hide">
+                        <td class="pt-3-half" contenteditable="true">Example</td>
+                        <td class="pt-3-half" contenteditable="true">Example</td>
+                        <td class="pt-3-half" contenteditable="true">Example</td>
+                        <td class="pt-3-half" contenteditable="true">Example</td>
+                        <td class="pt-3-half" contenteditable="true">Example</td>
+                        <td class="pt-3-half">
+                            <span class="table-up"><a href="#!" class="indigo-text"><i class="fas fa-long-arrow-alt-up" aria-hidden="true"></i></a></span>
+                            <span class="table-down"><a href="#!" class="indigo-text"><i class="fas fa-long-arrow-alt-down" aria-hidden="true"></i></a></span>
+                        </td>
+                        <td>
+                            <span class="table-remove"><button type="button" class="btn btn-danger btn-rounded btn-sm my-0 waves-effect waves-light">Remove</button></span>
+                        </td>
+                        </tr>`;
+
+                        $('.table-add').on('click', 'i', () => {
+
+                        const $clone = $tableID.find('tbody tr').last().clone(true).removeClass('hide table-line');
+
+                        if ($tableID.find('tbody tr').length === 0) {
+
+                            $('tbody').append(newTr);
+                        }
+
+                        $tableID.find('table').append($clone);
+                        });
+
+                        $tableID.on('click', '.table-remove', function () {
+
+                        $(this).parents('tr').detach();
+                        });
+
+                        $tableID.on('click', '.table-up', function () {
+
+                        const $row = $(this).parents('tr');
+
+                        if ($row.index() === 0) {
+                            return;
+                        }
+
+                        $row.prev().before($row.get(0));
+                        });
+
+                        $tableID.on('click', '.table-down', function () {
+
+                        const $row = $(this).parents('tr');
+                        $row.next().after($row.get(0));
+                        });
+
+                        // A few jQuery helpers for exporting only
+                        jQuery.fn.pop = [].pop;
+                        jQuery.fn.shift = [].shift;
+
+                        $BTN.on('click', () => {
+
+                        const $rows = $tableID.find('tr:not(:hidden)');
+                        const headers = [];
+                        const data = [];
+
+                        // Get the headers (add special header logic here)
+                        $($rows.shift()).find('th:not(:empty)').each(function () {
+
+                            headers.push($(this).text().toLowerCase());
+                        });
+
+                        // Turn all existing rows into a loopable array
+                        $rows.each(function () {
+                            const $td = $(this).find('td');
+                            const h = {};
+
+                            // Use the headers from earlier to name our hash keys
+                            headers.forEach((header, i) => {
+
+                            h[header] = $td.eq(i).text();
+                            });
+
+                            data.push(h);
+                        });
+
+                        // Output the result
+                        $EXPORT.text(JSON.stringify(data));
+                        });
+                        </script>
                     </div>
                     <div class="tab-pane fade" id="material" role="tabpanel" aria-labelledby="material-tab">
                         <h1>MATERIAL</h1>
                         <form  method="POST" action="{{ route('materiales.store') }}" enctype="multipart/form-data">
                             {{ csrf_field() }}
                             <div>
-                                Lista de Materiales 
+                                Lista de Materiales
                             </div>
 
-                            <div class="file-field">
-                                <div class="btn btn-asm">
+                            <div class="file-field ">
+                                <div class="btn btn-asm col-lg-12 col-md-12 col-sm-12 col-xs-12">
                                 <span>Escoger un Archivo</span>
                                 <input type="file" name="url">
                                 <input type="hidden" value="{{$curso->id}}" name="curso_id">
