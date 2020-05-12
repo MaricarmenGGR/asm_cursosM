3ecaabf568d3b821cb551ada6987c2d60044d262
Carlos Martínez <carlossmartinez98@gmail.com>
2020-05-11 20:45:48 -0500

    correcion cards

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,41 +4,39 @@
 <!-- VISTA COMO ADMINISTRADOR -->
 @if( Auth::user()->role->id == 1 )
     <div class="container-fluid">
-        <div class="cartas-curso"> 
+        <!--<div class="cartas-curso"> --><div class="row">
+
             @foreach($cursos as $curso)
-                <div class="carta-cursos">
-                    <div class="carta-curso_top">
-                        <img class="carta-curso_imagen" src="../uploads/{{$curso->imagenCurso}}">
-                    </div>
-                    <div class="carta-curso_contenido">
-                        <div class="carta-curso_fecha justify-content-end">
-                                <i class="fas fa-calendar-alt"></i>&nbsp;&nbsp;
-                                <time>{!! $curso->fechaInicio !!}</time>
+                <div class="col-sm-6 col-md-4 col-lg-3 mt-4">
+                    <div class="card">
+                        <img class="card-img-top" src="../uploads/{{$curso->imagenCurso}}">
+                        <div class="card-block">
+                            <h4 class="card-title" style="text-align: center;">{!! $curso->nombreCurso !!}</h4>
+                            <div class="meta" style="text-align: left; color: gray;">
+                                <i class="fas fa-calendar-alt"></i>&nbsp;&nbsp;{!! $curso->fechaInicio !!}
+                            </div>
+                            <div class="card-text" style="text-align: justify; color: black;">
+                                {!! $curso->descripcionCurso !!}
+                            </div>
                         </div>
-                        <h4 class="card-title">{!! $curso->nombreCurso !!}</h4>
-                        <hr>
-                        <p style="font-size: 12px;text-align: justify;">
-                            {!! $curso->descripcionCurso !!}
-                        </p>
+                        <div class="card-footer">
+                            <a class="btn btn-asm btn-block" href="/cursos/{{$curso->id}}" >
+                                Editar
+                            </a>
+                        </div>
                     </div>
-                    <a href="/cursos/{{$curso->id}}" class="black-text d-flex justify-content-start" style="text-decoration: inherit;">
-                        <h5>Editar&nbsp;<i class="fas fa-wrench"></i></h5>
-                    </a>
-                    <br>
                 </div>
             @endforeach
-                <div class="carta-cursos">
-                    <a href={{ route("crearCurso") }} style="text-decoration:none; color: rgb(40, 146, 157);" >
-                    
-                        <br><br>
-                        <div class="carta-curso_top">
-                            <img class="carta-curso_imagen" src="../img/nuevo_curso.png">
+
+                <div class="col-sm-6 col-md-4 col-lg-3 mt-4">
+                    <div class="card">
+                        <div class="card-block">
+                            <div class="card-text">
+                                <img class="carta-curso_imagen" src="../img/nuevo_curso.png">
+                                <p style="font-size: 20px;text-align: center; color: rgb(40, 146, 157);">Nuevo Curso</p>
+                            </div>
                         </div>
-                        <div class="carta-curso_contenido text-center">
-                            <p style="font-size: 20px;">Nuevo Curso</p>
-                        </div>
-                        <br>
-                    </a>
+                    </div>
                 </div>
         </div>
     </div>
@@ -47,57 +45,48 @@
 <!-- VISTA COMO USUARIO -->
 @if( Auth::user()->role->id == 2 )
     <div class="container-fluid">
-        <div class="cartas-curso">
+        <div class="row">
         @foreach($cursos as $curso)
             @if($curso->verificarArea($curso->id, Auth::user()->area_id ))
-                <div class="carta-cursos">
-                    <div class="carta-curso_top">
-                        <img class="carta-curso_imagen" src="../uploads/{{$curso->imagenCurso}}">
-                    </div>
-                    <div class="carta-curso_contenido">
-                        <div class="carta-curso_fecha justify-content-end">
-                                <i class="fas fa-calendar-alt"></i>&nbsp;&nbsp;
-                                <time>{!! $curso->fechaInicio !!}</time>
+                <div class="col-sm-6 col-md-4 col-lg-3 mt-4">
+                    <div class="card">
+                        <img class="carta-curso_imagen" style="width:100%;" src="../uploads/{{$curso->imagenCurso}}">
+                        <div class="card-block">
+                            <h4 class="card-title" style="text-align: center;">{!! $curso->nombreCurso !!}</h4>
+                            <div class="meta" style="text-align: left; color: gray;">
+                                <i class="fas fa-calendar-alt"></i>&nbsp;&nbsp;{!! $curso->fechaInicio !!}
+                            </div>
+                            <div class="card-text" style="text-align: justify; color: black;">
+                                {!! $curso->descripcionCurso !!}
+                            </div>
+                            @if( ! Auth::user()->estaInscrito($curso->id) )
+                            <a href="{{ route('cursosUsuario.show',$curso->id) }}" class="black-text d-flex justify-content-end">
+                                <h5>Ver más <i class="fas fa-angle-double-right"></i></h5>
+                            </a>
+                            @endif
                         </div>
-                        <h4 class="card-title">{!! $curso->nombreCurso !!}</h4>
-                        <hr>
-                        <p style="font-size: 12px;text-align: justify;">
-                            {!! $curso->descripcionCurso !!}
-                        </p>
-                    </div>
-                    @if( ! Auth::user()->estaInscrito($curso->id) )
-                    <a href="{{ route('cursosUsuario.show',$curso->id) }}" class="black-text d-flex justify-content-end">
-                        <h5>Ver más <i class="fas fa-angle-double-right"></i></h5>
-                    </a>
-                    @endif
-                    <div class="row">
-                        <div class="col-lg-12">
+                        <div class="card-footer">
                             @if( ! Auth::user()->estaInscrito($curso->id) && $curso->hayCupo($curso->id, Auth::user()->area_id) )
-                            <button type="button" class="btn btn-block btn-asm" onclick="inscripcion({{$curso->id}})">
+                            <button type="button" class="btn btn-asm btn-block" onclick="inscripcion({{$curso->id}})">
                                 Inscribirme
                             </button>
-                            <br>
                             @elseif( Auth::user()->estaInscrito($curso->id) )
-                            <a href="{{ route('cursosUsuario.show',$curso->id) }}" class="btn btn-block btn-asm">
+                            <a href="{{ route('cursosUsuario.show',$curso->id) }}" class="btn btn-asm btn-block">
                                 Entrar al curso
                             </a>
-                            <br>
                             @elseif( ! $curso->hayCupo($curso->id, Auth::user()->area_id) )
-                            <button disabled type="button" class="btn btn-block btn-asm">
+                            <button disabled type="button" class="btn btn-asm btn-block">
                                 Cupo lleno
                             </button>
                             @endif
                         </div>
                     </div>
-
+                </div>
                     <form id="frmInscribirse_{{$curso->id}}" method="post" action="/inscribirse">
                         @csrf
                         <input type="number" name="idCurso" value="{{$curso->id}}" hidden>
                     </form>
                     <input type="text" id="nombreCurso_{{$curso->id}}" value="{{$curso->nombreCurso}}" hidden>
-                    <br>
-                </div>
-
             @endif
         @endforeach
         </div>
